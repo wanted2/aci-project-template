fdc466b2fb2e1495c940360adc2a72023be406d8
Tuan Nguyen-Anh <tuan.nguyenanh.brse@gmail.com>
2020-12-02 10:56:23 +0000

    added a trigger template which can be used to reduce idle time

---
+++ functions/trigger/main.py
@@ -0,0 +1,16 @@
+import logging
+import azure.functions as func
+
+
+def main(event: func.EventHubEvent):
+    logging.info(f'Function triggered to process a message: {event.get_body().decode()}')
+    logging.info(f'  EnqueuedTimeUtc = {event.enqueued_time}')
+    logging.info(f'  SequenceNumber = {event.sequence_number}')
+    logging.info(f'  Offset = {event.offset}')
+
+    # Metadata
+    for key in event.metadata:
+        logging.info(f'Metadata: {key} = {event.metadata[key]}')
+    
+    # calling the ACI here only when needed to reduce idle time.
+    # See AC pricing model for details.
